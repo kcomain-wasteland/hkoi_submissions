bb7fc3762ee45e777c455fe0730215331433174d
kcomain <cheunghowui@gmail.com>
2019-10-08 23:23:15 +0800

    Automatic git commit from local at 20191008_23:23

---
+++ D101.py
@@ -0,0 +1,20 @@
+'''
+In this exercise, you will learn how to perform basic input and output. 
+Also, you will be able to try the HKOI Online Judge system.
+Write a program to read a 8-digit Hong Kong phone number and determine whether 
+it is a fixed line number (numbers that start with 2 or 3) or a mobile number 
+(numbers that start with 5, 6 or 9).
+'''
+a = input()
+aw = a[:1]
+
+if aw == '2':
+    print('Fixed')
+elif aw == '3':
+    print('Fixed')
+if aw == '5':
+    print('Mobile')
+elif aw == '6':
+    print('Mobile')
+elif aw == '9':
+    print('Mobile')
---
+++ D108.py
@@ -0,0 +1,10 @@
+'''
+Write a program to evaluate a simple arithmetic expression.
+The specifications of the expression are as follows:
+
+The expression consists of 3 operands and 2 operators.
+The operands are integers between 0 and 100 inclusive.
+The operators are either +(plus), -(minus) or *(times).
+The expression does not contain any brackets.
+'''
+# So DIFFICULT AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
