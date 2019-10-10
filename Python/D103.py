65a9dd0ac0e43690ef91e552a7f90a5452904718
kcomain <cheunghowui@gmail.com>
2019-10-10 20:53:59 +0800

    Automatic git commit from local at 20191010_20:53

--- Python/D103.py
+++ Python/D103.py
@@ -1,11 +1,14 @@
 '''Area of triangle(D103)
 There are many ways to calculate the area of a triangle.
 '''
-from math import sin
+from math import sin,degrees
 a = int(input())
 b = int(input())
 cdeg = int(input())
-d = (str((1/2)*a*b)+'{}'.format(sin(cdeg)))
-e = round(d,3)
+print(degrees(sin(cdeg)))
+d = 0.5 * a * b * degrees(sin(cdeg))
+# print('Cdeg sin: {}'.format(degrees(sin(cdeg))))
+# print('d = {}'.format(d))
+e = round(float(d),3)
 print(e)
 # 1/2 ab sin c
