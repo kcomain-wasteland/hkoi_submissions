18558206634c371bd5d27f0a44df8f2c43d62245
kcomain <cheunghowui@gmail.com>
2019-10-10 21:50:08 +0800

    Automatic git commit from local at 20191010_21:50

---
+++ Python/D802.py
@@ -0,0 +1,4 @@
+'''Bubble sort(D802)
+Bubble Sort sorts an array by repeatly comparing adjacent elements, and swap them if required. 
+Implement the Bubble Sort sorting algorithm. Sort the given array in the specified order.
+'''
