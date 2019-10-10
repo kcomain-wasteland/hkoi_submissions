60fc6470ccc1317f25c01b121f227b47310da919
kcomain <cheunghowui@gmail.com>
2019-10-10 23:04:48 +0800

    Automatic git commit from local at 20191010_23:04

--- Python/D108.py
+++ Python/D108.py
@@ -1,4 +1,4 @@
-'''
+'''Simple calculator(D108)
 Write a program to evaluate a simple arithmetic expression.
 The specifications of the expression are as follows:
 
@@ -8,11 +8,71 @@ The operators are either +(plus), -(minus) or *(times).
 The expression does not contain any brackets.
 '''
 # So DIFFICULT AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-
+import re
 a = input()
 bst = [c for c in a]
 first = ''
 sec = ''
 for i in bst:
-    print(i+'\n')
-print(bst)
+    if first == '':
+        if i == '+':
+            first = '+'
+        elif i == '-':
+            first = '-'
+        elif i == '*':
+            first = '*'
+        elif i == '/':
+            first = '/'
+    else:
+        if i == '+':
+            sec = '+'
+        elif i == '-':
+            sec = '-'
+        elif i == '*':
+            sec = '*'
+        elif i == '/':
+            sec = '/'
+# print('First: {}'.format(first))
+# print('Second: {}'.format(sec))
+regex = r'[\+\-\*\/]'
+b = re.sub(regex,'',a)
+c = b.split(' ')    
+result = 0
+print(c)
+if first == '+':
+    if sec == '+':
+        result = int(c[0]) + int(c[2]) + int(c[4])
+    elif sec == '-':
+        result = int(c[0]) + int(c[2]) - int(c[4])
+    elif sec == '*':
+        result = int(c[0]) + int(c[2]) * int(c[4])
+    elif sec == '/':
+        result = int(c[0]) + int(c[2]) / int(c[4])
+elif first == '-':
+    if sec == '+':
+        result = int(c[0]) - int(c[2]) + int(c[4])
+    elif sec == '-':
+        result = int(c[0]) - int(c[2]) - int(c[4])
+    elif sec == '*':
+        result = int(c[0]) - int(c[2]) * int(c[4])
+    elif sec == '/':
+        result = int(c[0]) - int(c[2]) / int(c[4])
+elif first == '*':
+    if sec == '+':
+        result = int(c[0]) * int(c[2]) + int(c[4])
+    elif sec == '-':
+        result = int(c[0]) * int(c[2]) - int(c[4])
+    elif sec == '*':
+        result = int(c[0]) * int(c[2]) * int(c[4])
+    elif sec == '/':
+        result = int(c[0]) * int(c[2]) / int(c[4])
+elif first == '/':
+    if sec == '+':
+        result = int(c[0]) / int(c[2]) + int(c[4])
+    elif sec == '-':
+        result = int(c[0]) / int(c[2]) - int(c[4])
+    elif sec == '*':
+        result = int(c[0]) / int(c[2]) * int(c[4])
+    elif sec == '/':
+        result = int(c[0]) / int(c[2]) / int(c[4])
+print(result)
