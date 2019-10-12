b039ca8873a0ff604133e784f4d6c34bb53d2089
kcomain <cheunghowui@gmail.com>
2019-10-12 11:27:06 +0800

    Automatic git commit from local at 20191012_11:27

--- Python/D106.py
+++ Python/D106.py
@@ -1,20 +1,36 @@
 '''Ordinal number(D106)
 Write a program to the ordinal form of a number.
 '''
-a = str(input())
-b = [k for k in a]
-alen = len(a)
+from argparse import ArgumentParser
+par = ArgumentParser()
+par.add_argument("-n")
+a = par.parse_args()
+qqq = a.n
+
+# a = str(input())
+b = [k for k in qqq]
+alen = len(qqq)
+c = b[alen-1]
 res = ''
 # print(b)
 # print('\n{}'.format(b[alen-1]))
-if a == '11' or '12' or '13':
-    res = a + 'th'
-elif b[alen-1] == '1':
-    res = a + 'st'
-elif b[alen-1] == '2':
-    res = a + 'nd'
-elif b[alen-1] == '3':
-    res = a + 'rd'
+# print(c)
+if qqq == '11':
+    res = qqq+'th'
+elif qqq == '12':
+    res = qqq+'th'
+elif qqq == '13':
+    res = qqq+'th'
 else:
-    res = a + 'th'
-print(res)
+    if c == '1':
+        res = qqq+'st'
+    elif c == '2':
+        res = qqq+'nd'
+    elif c == '3':
+        res = qqq+'rd'
+    else:
+        res = qqq+'th'
+# print(res)
+f = open("lastdep.txt", "a")
+f.write(res+'\n')
+f.close()
