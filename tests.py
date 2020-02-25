c03958b2d806a24aa16e4c2749ded548df619426
kcomain <cheunghowui@gmail.com>
2020-02-25 12:28:15 +0800

    Add tests

--- tests.py
+++ tests.py
@@ -1,7 +1,10 @@
-print('Loading...')
 import time
 import random
 import os
+print('Loading...')
+
+a = open('tests.sh', 'w')
+a.write('')
 time.sleep(random.random())
 print('Detecting Python Files...')
 files = []
@@ -9,23 +12,25 @@ sh = '#!/bin/bash\n'
 for i in os.listdir('Python'):
     if i[-3:] == '.py':
         print(i)
-        files.append('./Python/'+i)
+        files.append('./Python/' + i)
 
 for i in os.listdir('Python\\Finished'):
     if i[-3:] == '.py':
         print(i)
-        files.append('./Python/Finished/'+i)
+        files.append('./Python/Finished/' + i)
 
-print('Please Wait... Adding Files.')
+print('Adding Files...')
 count = 0
 for a in files:
-    print('Adding File ({}) [{} of {}]'.format(a,count+1,len(files)))
+    print('\rAdding File ({}) [{} of {}]'.format(a, count + 1, len(files)), end='')
     count += 1
-    sh += 'echo "Building File({})... [{} of {}]({}% finished)"\n'.format(a,count,len(files),((count)/len(files))*100)
+    sh += 'echo "Building File({})... [{} of {}]({}% finished)"\n'.format(a, count, len(files),
+                                                                          ((count) / len(files)) * 100)
     sh += 'python -S -m py_compile {}\n'.format(a)
-    time.sleep(random.random()/5)
-a = open('tests.sh','w')
+    time.sleep(random.random() / 5)
 sh += 'echo "Buils tests done. Thank you."'
+a = open('tests.sh', 'w')
+print("Writing File")
 a.write(sh)
 print('Process finished. Thanks for using.')
 a.close()
--- tests.sh
+++ tests.sh
@@ -1,28 +1,52 @@
 #!/bin/bash
-echo "Building File(./Python/D104.py)... [1 of 13](7.6923076923076925% finished)"
+echo "Building File(./Python/01014.py)... [1 of 25](4.0% finished)"
+python -S -m py_compile ./Python/01014.py
+echo "Building File(./Python/D104.py)... [2 of 25](8.0% finished)"
 python -S -m py_compile ./Python/D104.py
-echo "Building File(./Python/utils.py)... [2 of 13](15.384615384615385% finished)"
+echo "Building File(./Python/D201.py)... [3 of 25](12.0% finished)"
+python -S -m py_compile ./Python/D201.py
+echo "Building File(./Python/D204.py)... [4 of 25](16.0% finished)"
+python -S -m py_compile ./Python/D204.py
+echo "Building File(./Python/D304.py)... [5 of 25](20.0% finished)"
+python -S -m py_compile ./Python/D304.py
+echo "Building File(./Python/D305.py)... [6 of 25](24.0% finished)"
+python -S -m py_compile ./Python/D305.py
+echo "Building File(./Python/M17A9.py)... [7 of 25](28.000000000000004% finished)"
+python -S -m py_compile ./Python/M17A9.py
+echo "Building File(./Python/utils.py)... [8 of 25](32.0% finished)"
 python -S -m py_compile ./Python/utils.py
-echo "Building File(./Python/Finished/01005.py)... [3 of 13](23.076923076923077% finished)"
+echo "Building File(./Python/Finished/01005.py)... [9 of 25](36.0% finished)"
 python -S -m py_compile ./Python/Finished/01005.py
-echo "Building File(./Python/Finished/D100.py)... [4 of 13](30.76923076923077% finished)"
+echo "Building File(./Python/Finished/D100.py)... [10 of 25](40.0% finished)"
 python -S -m py_compile ./Python/Finished/D100.py
-echo "Building File(./Python/Finished/D101.py)... [5 of 13](38.46153846153847% finished)"
+echo "Building File(./Python/Finished/D101.py)... [11 of 25](44.0% finished)"
 python -S -m py_compile ./Python/Finished/D101.py
-echo "Building File(./Python/Finished/D102.py)... [6 of 13](46.15384615384615% finished)"
+echo "Building File(./Python/Finished/D102.py)... [12 of 25](48.0% finished)"
 python -S -m py_compile ./Python/Finished/D102.py
-echo "Building File(./Python/Finished/D103.py)... [7 of 13](53.84615384615385% finished)"
+echo "Building File(./Python/Finished/D103.py)... [13 of 25](52.0% finished)"
 python -S -m py_compile ./Python/Finished/D103.py
-echo "Building File(./Python/Finished/D105.py)... [8 of 13](61.53846153846154% finished)"
+echo "Building File(./Python/Finished/D105.py)... [14 of 25](56.00000000000001% finished)"
 python -S -m py_compile ./Python/Finished/D105.py
-echo "Building File(./Python/Finished/D106.py)... [9 of 13](69.23076923076923% finished)"
+echo "Building File(./Python/Finished/D106.py)... [15 of 25](60.0% finished)"
 python -S -m py_compile ./Python/Finished/D106.py
-echo "Building File(./Python/Finished/D108.py)... [10 of 13](76.92307692307693% finished)"
+echo "Building File(./Python/Finished/D108.py)... [16 of 25](64.0% finished)"
 python -S -m py_compile ./Python/Finished/D108.py
-echo "Building File(./Python/Finished/D109.py)... [11 of 13](84.61538461538461% finished)"
+echo "Building File(./Python/Finished/D109.py)... [17 of 25](68.0% finished)"
 python -S -m py_compile ./Python/Finished/D109.py
-echo "Building File(./Python/Finished/D301.py)... [12 of 13](92.3076923076923% finished)"
+echo "Building File(./Python/Finished/D301.py)... [18 of 25](72.0% finished)"
 python -S -m py_compile ./Python/Finished/D301.py
-echo "Building File(./Python/Finished/D802.py)... [13 of 13](100.0% finished)"
+echo "Building File(./Python/Finished/D302.py)... [19 of 25](76.0% finished)"
+python -S -m py_compile ./Python/Finished/D302.py
+echo "Building File(./Python/Finished/D402.py)... [20 of 25](80.0% finished)"
+python -S -m py_compile ./Python/Finished/D402.py
+echo "Building File(./Python/Finished/D501.py)... [21 of 25](84.0% finished)"
+python -S -m py_compile ./Python/Finished/D501.py
+echo "Building File(./Python/Finished/D701.py)... [22 of 25](88.0% finished)"
+python -S -m py_compile ./Python/Finished/D701.py
+echo "Building File(./Python/Finished/D702.py)... [23 of 25](92.0% finished)"
+python -S -m py_compile ./Python/Finished/D702.py
+echo "Building File(./Python/Finished/D802.py)... [24 of 25](96.0% finished)"
 python -S -m py_compile ./Python/Finished/D802.py
+echo "Building File(./Python/Finished/J104.py)... [25 of 25](100.0% finished)"
+python -S -m py_compile ./Python/Finished/J104.py
 echo "Buils tests done. Thank you."
